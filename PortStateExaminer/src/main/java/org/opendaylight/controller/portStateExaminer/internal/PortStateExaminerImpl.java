f9a2c87187fa28ef46c8e1feba70a7822f7ea20e
Swarvanu Sengupta <swarvanu007@gmail.com>
2015-01-02 23:49:19 +0530

    changes header

--- PortStateExaminer/src/main/java/org/opendaylight/controller/portStateExaminer/internal/PortStateExaminerImpl.java
+++ PortStateExaminer/src/main/java/org/opendaylight/controller/portStateExaminer/internal/PortStateExaminerImpl.java
@@ -1,18 +1,5 @@
 /*
- * Copyright (C) 2014 SDN Hub
-
- Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.
- You may not use this file except in compliance with this License.
- You may obtain a copy of the License at
-
-    http://www.gnu.org/licenses/gpl-3.0.txt
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- implied.
-
- *
+ * NEC Technologies India Ltd
  */
 
 package org.opendaylight.controller.portStateExaminer.internal;
